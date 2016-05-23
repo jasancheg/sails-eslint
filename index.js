b1bd8d78706da7a92e15a528ee0055c209493204
Jose Antonio Sanchez <jasancheg@gmail.com>
2016-05-22 22:12:22 -0600

    add eslint formatter pretty logs and watch for file changes

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+// oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+// WARNING: THIS HOOK USES PRIVATE, UNDOCUMENTED APIs THAT COULD CHANGE AT ANY TIME
+// oooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo
+// This hook uses an undocumented, private Sails core method in order to reload controllers without
+// having to lower/re-lift an app. You should not copy or reuse that code (clearly marked below) in an
+// app, because future releases of Sails--even patch releases--may cause it to stop functioning.
+// The private API usage used below will be replaced by a public method as soon as it is available.
+// In the meantime, enjoy, and as stated in the README, do not turn this hook on in production!
 var glob = require('glob');
 var path = require('path');
 var chalk = require('chalk');
+// Initialize eslint cli engine
 var CLI = require('eslint').CLIEngine;
-
 var cli = new CLI({});
 
-function runLint(pattern, format) {
+function runLint(dir, format) {
   var formatter = cli.getFormatter(format);
-  var report = cli.executeOnFiles([pattern]);
+  var report = cli.executeOnFiles([dir]);
 
   if (report && report.errorCount > 0) {
     console.log(
-      chalk.red('Code did not pass lint rules') +
+      chalk.red('error: Code did not pass lint rules') +
       formatter(report.results)
     );
   } else {
@@ -22,7 +30,7 @@ function runLint(pattern, format) {
 }
 
 module.exports = function (sails) {
-
+  console.log(process.cwd(), path.join(__dirname, 'node_modules', 'eslint-formatter-pretty'))
   return {
 
     /**
@@ -35,16 +43,33 @@ module.exports = function (sails) {
     defaults: {
 
       __configKey__: {
-        // Turn eslint checks on/off
-        check: true,
+        // Turn eslint on/off
+        active: true,
+        //use polling to watch file changes
+        //slower but sometimes needed for VM environments
+        usePolling: false,
         // choose which formatter to use
-        formatter: 'stylish',
-        // decide which folders/patterns should be checked
-        patterns: [
+        formatter: path.join(__dirname, 'node_modules', 'eslint-formatter-pretty'),
+        // decide which folders/dirs should be checked
+        dirs: [
           path.resolve(sails.config.appPath, 'api'),
           path.resolve(sails.config.appPath, 'config')
-        ]
+        ],
+        // Ignored paths, passed to anymatch
+        // String to be directly matched, string with glob patterns,
+        // regular expression test, function
+        // or an array of any number and mix of these types
+        ignored: []
       }
+    },
+
+    configure: function() {
+      sails.config[this.configKey].active =
+        // If an explicit value for the "active" config option is set, use it
+        (typeof sails.config[this.configKey].active !== 'undefined') ?
+          // Otherwise turn off in production environment, on for all others
+          sails.config[this.configKey].active :
+            (sails.config.environment != 'production');
     },
 
     /**
@@ -53,22 +78,47 @@ module.exports = function (sails) {
     */
     initialize: function (cb) {
 
+      var self = this;
+
+      // Initialize the file watcher to watch controller and model dirs
+      var chokidar = require('chokidar');
+      // Watch both the controllers and models directories
+      var watcher = chokidar.watch(sails.config[this.configKey].dirs, {
+        // Ignore the initial "add" events which are generated when Chokidar
+        // starts watching files
+        ignoreInitial: true,
+        usePolling: sails.config[this.configKey].usePolling,
+        ignored: sails.config[this.configKey].ignored
+      });
+
       // If the hook has been deactivated, just return
-      if (!sails.config[this.configKey].check) {
+      if (!sails.config[this.configKey].active) {
         sails.log.verbose('Eslint hook deactivated.');
         return cb();
       } else {
 
         var format = sails.config[this.configKey].formatter || 'stylish';
-        var patterns = sails.config[this.configKey].patterns || ['api', 'config'];
+        var dirs = sails.config[this.configKey].dirs || ['api', 'config'];
+        var formatDirs = [];
+        var paths = '';
 
-        patterns.forEach(function (pattern) {
-          if (glob.hasMagic(pattern)) {
-            glob.sync(pattern).forEach(function (file) {
+        if(sails.config[this.configKey].dirs) {
+          sails.config[this.configKey].dirs.forEach(function(item){
+            formatDirs.push('path: ' + item.replace(process.cwd(), ''));
+          });
+          paths = chalk.yellow('\n' + formatDirs.join('\n'));
+        }
+
+        sails.log.verbose("Eslint watching: ", sails.config[this.configKey].dirs);
+        sails.log.info(chalk.blue("Eslint watching:"), paths);
+
+        dirs.forEach(function (dir) {
+          if (glob.hasMagic(dir)) {
+            glob.sync(dir).forEach(function (file) {
               runLint(file, format);
             });
           } else {
-            runLint(pattern, format);
+            runLint(dir, format);
           }
         });
         return cb();
